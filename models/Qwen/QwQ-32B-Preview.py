0c354dced919b9eda59bbdaea5b69566bcb75b34
LuYF-Lemon-love <3555028709@qq.com>
2024-11-29 09:51:54 +0800

    xxx

---
+++ models/Qwen/QwQ-32B-Preview.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Qwen/QwQ-32B-Preview"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "How many r in strawberry."
+messages = [
+    {"role": "system", "content": "You are a helpful and harmless assistant. You are Qwen developed by Alibaba. You should think step-by-step."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
