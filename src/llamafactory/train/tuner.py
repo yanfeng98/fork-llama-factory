51ecaf6e31bf7e9e107ff02ddda4db6cdd67628b
LuYF-Lemon-love <3555028709@qq.com>
2024-12-01 22:27:40 +0800

    000

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -29,7 +29,6 @@ from .dpo import run_dpo
 from .kto import run_kto
 from .ppo import run_ppo
 from .pt import run_pt
-from .rm import run_rm
 from .sft import run_sft
 
 
@@ -48,8 +47,6 @@ def run_exp(args: Optional[Dict[str, Any]] = None, callbacks: List["TrainerCallb
         run_pt(model_args, data_args, training_args, finetuning_args, callbacks)
     elif finetuning_args.stage == "sft":
         run_sft(model_args, data_args, training_args, finetuning_args, generating_args, callbacks)
-    elif finetuning_args.stage == "rm":
-        run_rm(model_args, data_args, training_args, finetuning_args, callbacks)
     elif finetuning_args.stage == "ppo":
         run_ppo(model_args, data_args, training_args, finetuning_args, generating_args, callbacks)
     elif finetuning_args.stage == "dpo":
